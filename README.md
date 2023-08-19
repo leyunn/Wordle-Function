89f219c1d979b17748861f8aef8cadb09e0f3aa8
leyun <b09902088@csie.ntu.edu.tw>
2023-08-19 17:17:36 +0800

    upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Wordle Function
+
+>A simple serverless function to protect the API key used in [Wordle Prac](https://wordle-prac.vercel.app/)
+
+- Tools: Firebase
+
---
+++ functions/src/index.ts
@@ -0,0 +1,33 @@
+import {onRequest} from "firebase-functions/v2/https";
+import {defineString} from "firebase-functions/params";
+
+const apiKey = defineString("APIKEY");
+
+export const verifyWord = onRequest(async (req, res)=> {
+  try {
+    // // Set the allowed origin
+    const allowedOrigin = "https://wordle-prac.vercel.app";
+
+    // // Check if the request's origin matches the allowed origin
+    if (req.headers.origin !== allowedOrigin) {
+      res.status(403).send("Forbidden");
+    }
+    const word: string | undefined = req.query.word as string;
+    if (!word) {
+      res.status(400).send("Missing word");
+    }
+    const url = `https://api.wordnik.com/v4/word.json/${word}/definitions?limit=1&includeRelated=false&sourceDictionaries=all&useCanonical=true&includeTags=false&api_key=${apiKey.value()}`;
+    console.log(url);
+    const response = await fetch(url);
+    // // Set CORS headers to allow the specific origin
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+    if (response.status == 404) {
+      res.status(404).send("Word not found");
+    } else {
+      const json = await response.json();
+      res.status(200).send(json);
+    }
+  } catch (error) {
+    res.status(500).send("Error fetching data");
+  }
+});
